4788a407cc56ccd1b9e558221a187c6eebccdcd3
e183294h <e183294h@ubuntu-info-19>
2019-11-06 11:55:55 +0100

    Code UML en svg

--- TestSVG/src/svg.java
+++ TestSVG/src/svg.java
@@ -5,10 +5,28 @@ import org.w3c.dom.Document;
 import org.apache.batik.svggen.SVGGraphics2D;
 import org.apache.batik.dom.GenericDOMImplementation;
 public class svg {
+	int cmp = 0;
 	
    public void paint(SVGGraphics2D svgGenerator) {
-      svgGenerator.setPaint(Color.red);
-      svgGenerator.fill(new Rectangle(10, 10, 100, 100));
+      svgGenerator.setPaint(Color.BLACK);
+      int y;
+      y = cmp / 3 ;
+      svgGenerator.drawRect(10+(300*(cmp%3)), 10+(300*y), 200, 40);
+      svgGenerator.drawString("Eleve", 90+(300*(cmp%3)), 35+(300*y));
+      svgGenerator.drawRect(10+(300*(cmp%3)), 50+(300*y), 200, 100);
+      svgGenerator.drawString("# Nom : String", 30+(300*(cmp%3)), 70+(300*y));
+      svgGenerator.drawString("# Prenom : String", 30+(300*(cmp%3)), 85+(300*y));
+      svgGenerator.drawString("# Âge : Integer", 30+(300*(cmp%3)), 100+(300*y));
+      svgGenerator.drawString("# ddn : Date", 30+(300*(cmp%3)), 115+(300*y));
+      svgGenerator.drawString("# numEtu : Integer", 30+(300*(cmp%3)), 130+(300*y));
+      svgGenerator.drawString("# Moyenne : float", 30+(300*(cmp%3)), 145+(300*y));
+      svgGenerator.drawRect(10+(300*(cmp%3)), 150+(300*y), 200, 80);
+      svgGenerator.drawString("+ getNumEtu() : Integer", 30+(300*(cmp%3)), 165+(300*y));
+      svgGenerator.drawString("+ getNomPrenom() : String", 30+(300*(cmp%3)), 180+(300*y));
+      svgGenerator.drawString("+ getDDN() : Date()", 30+(300*(cmp%3)),195+(300*y));
+      svgGenerator.drawString("+ getClasse() : Class()", 30+(300*(cmp%3)), 210+(300*y));
+      svgGenerator.drawString("+ toString() : String", 30+(300*(cmp%3)), 225+(300*y));
+      cmp ++;
    }
    
    public static void main(String [] args) throws IOException {
@@ -26,8 +44,17 @@ public class svg {
       /* Dessine sur le composant svggenerator */
       svg test;
       test = new svg();
+      svgGenerator.setPaint(Color.white);
+	  svgGenerator.fill(new Rectangle(0,0,1000,1000));
+	  test.paint(svgGenerator);
       test.paint(svgGenerator);
-
+      test.paint(svgGenerator);
+      test.paint(svgGenerator);
+      test.paint(svgGenerator);
+      test.paint(svgGenerator);
+      test.paint(svgGenerator);
+      test.paint(svgGenerator);
+      test.paint(svgGenerator);
       /* sortir le résultat (peut se faire sur n'importe quel java.io.writer) */
       svgGenerator.stream("Image_TestSVGGen.svg");
    }
