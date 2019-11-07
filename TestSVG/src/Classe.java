097c0e4db7c29fea6dc51fe40ce9ae3ae0a369ea
hcorentin <hcorentin@hcorentin-PC>
2019-11-07 09:21:42 +0100

    Modif pour test package et liaison

--- TestSVG/src/Classe.java
+++ TestSVG/src/Classe.java
@@ -6,6 +6,17 @@ public class Classe {
 	private String name;
 	private List<String> attribut;
 	private List<String> methode;
+	private List<Classe> liaison;
+	
+	public Classe(String name, List<String> attribut, List<String> methode, List<Classe> liaison, String paquage) {
+		super();
+		this.name = name;
+		this.attribut = attribut;
+		this.methode = methode;
+		this.liaison = liaison;
+		this.paquage = paquage;
+	}
+	private String paquage;
 	
 	public Classe(String name, List<String> attribut, List<String> methode) {
 		super();
@@ -30,5 +41,17 @@ public class Classe {
 	}
 	public void setMethode(List<String> methode) {
 		this.methode = methode;
+	}
+	public String getPaquage() {
+		return paquage;
+	}
+	public void setPaquage(String paquage) {
+		this.paquage = paquage;
+	}
+	public List<Classe> getLiaison() {
+		return liaison;
+	}
+	public void setLiaison(List<Classe> liaison) {
+		this.liaison = liaison;
 	}
 }
