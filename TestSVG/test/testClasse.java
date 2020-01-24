e50e02fb0b0bdfd36083dbaa6e5442e338170a64
e183294h <e183294h@ubuntu-info-19>
2020-01-24 09:35:03 +0100

    Import des test

---
+++ TestSVG/test/testClasse.java
@@ -0,0 +1,28 @@
+package test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+import scanner.Classe;
+import srcALire.Cours.Enseignant;
+
+public class testClasse {
+    @Test
+    public void classe(){
+        assertEquals("Enseignant :\n" +
+                "\n" +
+                "- enseignement : List\n" +
+                "- nom : String \n" +
+                "\n" +
+                "+ ajoutMatiere(: Matiere) : void \n"+
+                "+ retirerMatiere(: Matiere) : void \n",new Classe(new Enseignant("Jean-François Remm").getClass()).toString());
+    }
+    @Test
+    public void testpackage(){
+        assertEquals("Cours ",new Classe(new Enseignant("Jean-François Remm").getClass()).getPackage());
+    }
+    @Test
+    public void testnom(){
+        assertEquals("Enseignant ",new Classe(new Enseignant("Jean-François Remm").getClass()).getNom());
+    }
+}
---
+++ TestSVG/test/testLien.java
@@ -0,0 +1,47 @@
+package test;
+import org.junit.jupiter.api.Test;
+import scanner.Classe;
+import scanner.Lien;
+import srcALire.Cours.Enseignant;
+import srcALire.Cours.Matiere;
+import srcALire.Groupe.Etudiant;
+import srcALire.Groupe.groupe;
+
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+public class testLien {
+    groupe gr = new groupe(1,20);
+    Classe prof = new Classe(new Enseignant("Jean-François Remm").getClass());
+    Classe matiere = new Classe(new Matiere("Programmation objet 2",18).getClass());
+    Classe groupe = new Classe(gr.getClass());
+    Classe etudiant = new Classe(new Etudiant("De Lapatefeuilleté","Hubert","e185475j",gr).getClass());
+    @Test
+    public void trouverLien(){
+
+        Lien li =new Lien(prof);
+        assertTrue(li.presenceLien(matiere));
+        assertEquals("*",li.getMultipliciteD());
+        assertEquals("1",li.getMultipliciteF());
+    }
+    @Test
+    public void testnonLien(){
+
+        Lien li =new Lien(prof);
+        assertFalse(li.presenceLien(etudiant));
+        assertEquals("0",li.getMultipliciteD());
+        assertEquals("0",li.getMultipliciteF());
+    }
+    @Test
+    public void testMultiplicité(){
+        Lien li = new Lien(matiere);
+        li.presenceLien(groupe);
+        assertEquals("0",li.getMultipliciteD());
+        assertEquals("*",li.getMultipliciteF());
+
+    }
+
+
+
+}
