58eb2a220ca6bbd2b1faf4919b62675acf494817
e183294h <e183294h@ubuntu-info-19>
2020-02-20 11:06:07 +0100

    Maj correct indentation

--- TestSVG/scanner/Application.java
+++ TestSVG/scanner/Application.java
@@ -14,54 +14,54 @@ import srcALire.Cours.*;
 import srcALire.Groupe.*;
 
 public class Application {
-	
+
 	/**
 	 * Fonction qui dessine un diagramme UML sur une image svg via les donnée passé dans la liste de classe
 	 * @param l liste des classes à dessiner
 	 * @param name nom du fichier .svg à générer
 	 */
-//	public static void UML(List<Classe> l, String name) {
-//		 List<uml.Classe> listC = new ArrayList<>();
-//	        for (Classe classe : l) {//traduction des Classe en src.Classe pour générer l'image UML
-//	        	List<String> lmeth = new ArrayList<>();
-//				for (String s : classe.getMethodes()) {
-//					lmeth.add(s);
-//				}
-//				List<String> latt = new ArrayList<>();
-//				for (Argument att : classe.getAttributs()) {
-//					latt.add(att.toString());
-//				}
-//				uml.Classe c0 = new uml.Classe(classe.getNom(), latt, lmeth, classe.getPackage());
-//				listC.add(c0);
-//			}
-//	        for(Classe classe : l) {//génération des liens sur les classe src.Classe
-//	        	uml.Classe depart = null;
-//	        	for (uml.Classe classe2 : listC) {
-//					if(classe2.getName().equals(classe.getNom())) {
-//						depart = classe2;
-//					}
-//				}
-//	        	uml.Classe arriver = null;
-//	        	for (Lien lien : classe.getLiens()) {
-//	        		for (uml.Classe classe2 : listC) {
-//						if(classe2.getName().equals(lien.getLier().getNom())) {
-//							arriver = classe2;
-//						}
-//					}
-//	        	if(depart != null && arriver != null)
-//	        		depart.addLiaison(arriver);
-//				}
-//	        }
-//	     uml.svg.createUML(listC, name);//appel de la fonction pour créé l'UML
-//	}
-	
+	//	public static void UML(List<Classe> l, String name) {
+	//		 List<uml.Classe> listC = new ArrayList<>();
+	//	        for (Classe classe : l) {//traduction des Classe en src.Classe pour générer l'image UML
+	//	        	List<String> lmeth = new ArrayList<>();
+	//				for (String s : classe.getMethodes()) {
+	//					lmeth.add(s);
+	//				}
+	//				List<String> latt = new ArrayList<>();
+	//				for (Argument att : classe.getAttributs()) {
+	//					latt.add(att.toString());
+	//				}
+	//				uml.Classe c0 = new uml.Classe(classe.getNom(), latt, lmeth, classe.getPackage());
+	//				listC.add(c0);
+	//			}
+	//	        for(Classe classe : l) {//génération des liens sur les classe src.Classe
+	//	        	uml.Classe depart = null;
+	//	        	for (uml.Classe classe2 : listC) {
+	//					if(classe2.getName().equals(classe.getNom())) {
+	//						depart = classe2;
+	//					}
+	//				}
+	//	        	uml.Classe arriver = null;
+	//	        	for (Lien lien : classe.getLiens()) {
+	//	        		for (uml.Classe classe2 : listC) {
+	//						if(classe2.getName().equals(lien.getLier().getNom())) {
+	//							arriver = classe2;
+	//						}
+	//					}
+	//	        	if(depart != null && arriver != null)
+	//	        		depart.addLiaison(arriver);
+	//				}
+	//	        }
+	//	     uml.svg.createUML(listC, name);//appel de la fonction pour créé l'UML
+	//	}
+
 	public static void fichier(List<Classe> l, String name) {
 		List<Package> pack = new ArrayList<>();
 		Map<String,Boolean> tab = new HashMap<>();
 		for (Classe cl : l) {
 			tab.put(cl.getNom(),false);
 		}
-        Application.triClasse(pack, l);
+		Application.triClasse(pack, l);
 		try {
 			Writer write = new FileWriter(name+".txt");
 			write.write("@startuml\n");
@@ -86,12 +86,12 @@ public class Application {
 			for (Package paqu : pack) {
 				write.write("package \""+paqu.getName()+"\"{\n");
 				for (Classe classe : paqu.getList()) {
-					
+
 					if(classe.isInterface())
-						write.write("interface "+classe.getNom()+"{\n");
+						write.write("interface "+classe.getNom()+" {\n");
 					else
-						write.write("class "+classe.getNom()+"{\n");
-					
+						write.write("class "+classe.getNom()+" {\n");
+
 					for (Argument s : classe.getAttributs()) {
 						write.write("{field} "+s.toString()+"\n");
 					}
@@ -106,7 +106,7 @@ public class Application {
 				List<Lien> liens = classe.getLiens();
 				for (Lien lien : liens) {
 					if( !tab.get( lien.getLier().getNom()) ) {
-					write.write("\n"+classe.getNom() + "\""+lien.getMultipliciteF()+"\" -- \""+lien.getMultipliciteD() +"\"" + lien.getLier().getNom()+"\n");
+						write.write("\n"+classe.getNom() + "\""+lien.getMultipliciteF()+"\" -- \""+lien.getMultipliciteD() +"\"" + lien.getLier().getNom()+"\n");
 					}
 				}
 				tab.replace(classe.getNom(), true);
@@ -125,7 +125,7 @@ public class Application {
 			e.printStackTrace();
 		}
 	}
-	
+
 	private static void triClasse(List<Package> pack, List<Classe> list) {
 		for(Classe classe : list) {//parcours des classes
 			if(pack.size()>0) {//si la liste n'est pas vide
@@ -142,7 +142,7 @@ public class Application {
 					Package p = new Package(classe.getPackage());
 					p.addClasse(classe);
 					pack.add(p);
-			}
+				}
 			}else {//si elle est vide alors on créé le premier package
 				Package p = new Package(classe.getPackage());
 				p.addClasse(classe);
@@ -151,38 +151,38 @@ public class Application {
 		}
 	}
 
-    public static void main(String[] args) {
+	public static void main(String[] args) {
 
-        System.out.println("-----------------Test avec du vrai code---------------------");
-        groupe gr = new groupe(1,20);
-        Classe prof = new Classe(new Enseignant("Jean-François Remm").getClass());
-        Classe matiere = new Classe(new Matiere("Programmation objet 2",18).getClass());
-        Classe groupe = new Classe(gr.getClass());
-        Classe etudiant = new Classe(new Etudiant("De Lapatefeuilleté","Hubert","e185475j",gr).getClass());
-        Classe iut = new Classe(new IUT(8, 109, 30, 14).getClass());
-        Classe pers = new Classe(new Personne("gilbert","montagné").getClass());
-        Classe matiereI = new Classe(MatiereI.class);
-        matiereI.interF();
-        List<Classe> li = new LinkedList<>();
-        etudiant.addExt(pers);
-        matiere.addImpl(matiereI);
-        li.add(prof);
-        li.add(matiere);
-        li.add(groupe);
-        li.add(etudiant);
-        li.add(iut);
-        li.add(pers);
-        li.add(matiereI);
-//        System.out.println(prof.toString());
-//        System.out.println(matiere.toString());
-//        System.out.println(groupe.toString());
-//        System.out.println(etudiant.toString());
-        for(int i =0;i<li.size();i++){
-            li.get(i).trouverLien(li);
-        }
-        Application.fichier(li, "Bonjour");
-        //Application.UML(li, "test-fusion-1");
-    }
+		System.out.println("-----------------Test avec du vrai code---------------------");
+		groupe gr = new groupe(1,20);
+		Classe prof = new Classe(new Enseignant("Jean-François Remm").getClass());
+		Classe matiere = new Classe(new Matiere("Programmation objet 2",18).getClass());
+		Classe groupe = new Classe(gr.getClass());
+		Classe etudiant = new Classe(new Etudiant("De Lapatefeuilleté","Hubert","e185475j",gr).getClass());
+		Classe iut = new Classe(new IUT(8, 109, 30, 14).getClass());
+		Classe pers = new Classe(new Personne("gilbert","montagné").getClass());
+		Classe matiereI = new Classe(MatiereI.class);
+		matiereI.interF();
+		List<Classe> li = new LinkedList<>();
+		etudiant.addExt(pers);
+		matiere.addImpl(matiereI);
+		li.add(prof);
+		li.add(matiere);
+		li.add(groupe);
+		li.add(etudiant);
+		li.add(iut);
+		li.add(pers);
+		li.add(matiereI);
+		//        System.out.println(prof.toString());
+		//        System.out.println(matiere.toString());
+		//        System.out.println(groupe.toString());
+		//        System.out.println(etudiant.toString());
+		for(int i =0;i<li.size();i++){
+			li.get(i).trouverLien(li);
+		}
+		Application.fichier(li, "Bonjour");
+		//Application.UML(li, "test-fusion-1");
+	}
 
 
 }
