80f9ffdda61088016b6003f87c53957367ab0a8f
e183294h <e183294h@ubuntu-info-19>
2020-02-20 11:24:32 +0100

    MAJ javaDoc

--- TestSVG/scanner/Application.java
+++ TestSVG/scanner/Application.java
@@ -55,17 +55,22 @@ public class Application {
 	//	     uml.svg.createUML(listC, name);//appel de la fonction pour créé l'UML
 	//	}
 
+	/**
+	 * Fonction qui créé le fichier contenant le plantuml
+	 * @param l la liste des classes dont on va créer le diagramme en plantuml 
+	 * @param name le nom du fichiers .txt qui sera créer contenant le plantuml
+	 */
 	public static void fichier(List<Classe> l, String name) {
 		List<Package> pack = new ArrayList<>();
 		Map<String,Boolean> tab = new HashMap<>();
-		for (Classe cl : l) {
+		for (Classe cl : l) {//création d'une hashmap qui est util pour empecher le dédoublement des liens
 			tab.put(cl.getNom(),false);
 		}
-		Application.triClasse(pack, l);
+		Application.triClasse(pack, l);//trie des packages
 		try {
-			Writer write = new FileWriter(name+".txt");
-			write.write("@startuml\n");
-			/* Exemple module
+			Writer write = new FileWriter(name+".txt");//ouverture du fichiers de sortit
+			write.write("@startuml\n");//début du plantuml
+			/* Exemple plantuml
 			 * @startuml
 				package "Classic Collections" #DDDDDD {
 				class Dummy1 {
@@ -83,49 +88,54 @@ public class Application {
 				Dummy3 -- Dummy1
 				@enduml
 			 */
-			for (Package paqu : pack) {
-				write.write("package \""+paqu.getName()+"\"{\n");
-				for (Classe classe : paqu.getList()) {
+			for (Package paqu : pack) {//Parcours les packages
+				write.write("package \""+paqu.getName()+"\"{\n");//création du package en plantuml
+				for (Classe classe : paqu.getList()) {//parcours les classe
 
-					if(classe.isInterface())
+					if(classe.isInterface())//affichage si la classe est une interface ou une classe
 						write.write("interface "+classe.getNom()+" {\n");
 					else
 						write.write("class "+classe.getNom()+" {\n");
 
-					for (Argument s : classe.getAttributs()) {
+					for (Argument s : classe.getAttributs()) {//affichage des attributs
 						write.write("{field} "+s.toString()+"\n");
 					}
-					for (String s : classe.getMethodes()) {
+					for (String s : classe.getMethodes()) {//affichage des mathodes
 						write.write("{method} "+s.toString()+"\n");
 					}
-					write.write("}\n");
+					write.write("}\n");//ferme la classe
 				}
-				write.write("}\n");
+				write.write("}\n");//ferme le package
 			}
-			for (Classe classe : l) {
+			for (Classe classe : l) {//affichage des liens entre classe avec leur multiplicité
 				List<Lien> liens = classe.getLiens();
 				for (Lien lien : liens) {
-					if( !tab.get( lien.getLier().getNom()) ) {
+					if( !tab.get( lien.getLier().getNom()) ) {//vérifie si le lien n'a pas déjà été fait dans un sens
 						write.write("\n"+classe.getNom() + "\""+lien.getMultipliciteF()+"\" -- \""+lien.getMultipliciteD() +"\"" + lien.getLier().getNom()+"\n");
 					}
 				}
-				tab.replace(classe.getNom(), true);
-				if(classe.isExtend()) {
+				tab.replace(classe.getNom(), true);//indique que le lien a déjà été fait dans un sens
+				if(classe.isExtend()) {//gère les extends
 					write.write("\n"+classe.getNom()+" --|> "+classe.getExt().getNom()+"\n");
 				}
-				if(classe.isImplement()) {
+				if(classe.isImplement()) {//gère les implementation
 					for (Classe ck : classe.getImpl()) {
 						write.write("\n"+classe.getNom()+" ..|> "+ck.getNom()+"\n");
 					}
 				}
 			}
-			write.write("\n@enduml");
-			write.close();
+			write.write("\n@enduml");//ferme le plantuml
+			write.close();//ferme le Writer du fichiers
 		} catch (IOException e) {
-			e.printStackTrace();
+			System.out.println(e+" "+e.getMessage());
 		}
 	}
 
+	/**
+	 * Fonction qui trie les classes dans une liste de leur package
+	 * @param pack la list des package ( entrée/sortie ) vide au départ puis en sortit elle contiendra les package et leur classe trié
+	 * @param list
+	 */
 	private static void triClasse(List<Package> pack, List<Classe> list) {
 		for(Classe classe : list) {//parcours des classes
 			if(pack.size()>0) {//si la liste n'est pas vide
