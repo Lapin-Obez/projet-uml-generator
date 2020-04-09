de57ddf7797431083577e26359314790e2803e21
poupl <gabriel.pouplin@etu.univ-nantes.fr>
2020-04-09 20:11:45 +0200

    projet automatisé fonctionnel

--- TestSVG/scanner/Application.java
+++ TestSVG/scanner/Application.java
@@ -63,7 +63,7 @@ public class Application {
 
 	/**
 	 * Fonction qui cr√©√© le fichier contenant le plantuml
-	 * @param l la liste des classes dont on va cr√©er le diagramme en plantuml 
+	 * @param l la liste des classes dont on va cr√©er le diagramme en plantuml
 	 * @param name le nom du fichiers .txt qui sera cr√©er contenant le plantuml
 	 */
 	public static void fichier(List<Classe> l, String name) {
@@ -161,7 +161,7 @@ public class Application {
 						}
 					}
 				}
-				if(!trv) {//sinon on cr√©√© le package 
+				if(!trv) {//sinon on cr√©√© le package
 					Package p = new Package(classe.getPackage());
 					p.addClasse(classe);
 					pack.add(p);
@@ -173,7 +173,7 @@ public class Application {
 			}
 		}
 	}
-	
+
 	public static List<Classe> chercherExtend(List<Classe> li,List<Class> cla){
 		for(int i=0;i<li.size();i++){
 			for (Class e:cla) {
@@ -204,44 +204,29 @@ public class Application {
 	}
 
 	public static void main(String[] args) {
-		
-		Scanner scanner=new Scanner(System.in);
-		System.out.println("Veuillez rentrer le chemin du dossier o˘ sont ranger les fichers java ‡ transformer en UML \n");
-		String chaine= scanner.nextLine();
-		scanner.close();
-		List<String> chemins= scanDoss(chaine);
-		List<Classe> lClass=new LinkedList<Classe>();
-		for(int i=0;i<chemins.size();i++) {
-			lClass.add(Application.scan(chemins.get(i).toString()));
-		}
-		
-		
-	//test pour l'affichage
-		System.out.println("-----------------Test avec de vraies classes---------------------");
-		groupe gr = new groupe(1,20);
-		Class pr = new Enseignant("Jean-Fran√ßois Remm").getClass();
-		Class ma =new Matiere("Programmation objet 2",18).getClass();
-		Class et =new Etudiant("De Lapatefeuillet√©","Hubert","e185475j",gr).getClass();
-		Class de = MatiereI.class;
-		Class s = Personne.class;
-		Class iut = IUT.class;
-		Class fi = Fichier.class;
-		Class dos = Dossier.class;
-		Class elem = Element.class;
-		
-		List<Class> cla = new LinkedList<>();
-		Class gro= gr.getClass();
+		List<String> chemins;
+		String c ="";
+		try {
+			 c = new java.io.File(".").getCanonicalPath();
 
-		cla.add(pr);
-		cla.add(ma);
-		cla.add(et);
-		cla.add(de);
-		cla.add(gro);
-		cla.add(s);
-		cla.add(iut);
-		cla.add(fi);
-		cla.add(dos);
-		cla.add(elem);
+		}catch(Exception e){
+
+		}
+		chemins = scanDoss(c+"\\srcALire");
+		List<Class> cla = new LinkedList<>();
+		//va cr√©er une instance de toute les classes envoy√©es
+		for (String s:chemins) {
+			try{
+				s= s.replace(".java","");
+				s = s.replace(c+"\\","");
+				s = s.replace("\\",".");
+				System.out.println(s);
+				Class test = Class.forName(s);
+				cla.add(test);
+			}catch(Exception e){
+				System.out.println(e.getMessage());
+			}
+		}
 
 		List<Classe> li = new LinkedList<>();
 		for (int i=0;i<cla.size();i++){
@@ -251,27 +236,23 @@ public class Application {
 			classe.trouverLien(li);
 		}
 		li = chercherExtend(li,cla);
-		
-		Scanner sc = new Scanner(System.in);
-		System.out.println("Veuillez saisir le nom du fichier ");//lecture pour determiner
-		String str = sc.nextLine();
-		sc.close();
-		Application.fichier(li, str);//appelle de la mÔøΩthode qui gÔøΩnÔøΩre un fichier qui contient le plantuml des classes donnÔøΩes en paramÔøΩtre
+
+		Application.fichier(li, "Resultat");//appelle de la m√©thode qui g√©n√®re un fichier qui contient le plantuml des classes donn√©es en param√®tre
 		//Application.UML(li, "test-fusion-1");
 	}
-	
+
 	public static List<String> scanDoss(String c) {
 		List<String> chemins=new LinkedList<>();
-		
+
 			File f=new File(c);
 			File[] fs=f.listFiles();
 			File[] fss;
 			for(int i=0;i<fs.length;i++) {
-				System.out.println(fs[i].toString());
+
 				if(!fs[i].toString().contains(".")) {
 					fss=fs[i].listFiles();
 					for(int j=0;j<fss.length;j++) {
-						System.out.println(fss[j].toString());
+
 						if(fss[j].toString().contains(".java")) {
 							chemins.add(fss[j].toString());
 						}else if(fss[j].toString().contains(".txt")) {
@@ -284,370 +265,9 @@ public class Application {
 					chemins.add(fs[i].toString());
 				}
 			}
-		
+
 		return chemins;
-		
-	}
 
-
-	public static Classe scan(String chemin) {
-		Scanner sc;
-		try {
-			sc = new Scanner(new File(chemin));
-			String s;
-			String imple=" ",thro= " ",extend = "";
-			List<String> impleList=new LinkedList<>();
-			List<String> throList=new LinkedList<>();
-			String[] thT,implT;
-			String nomC="";
-			List<String> methodes= new LinkedList<>();
-			List<String> attributs= new LinkedList<>();
-			boolean implement=false,abstrait=false,etend=false, Interface=false;
-			String pack=" ";
-			
-			/*Scan du fichier*/
-			while(sc.hasNextLine()) {
-				s=sc.nextLine();
-				s=s.trim();
-			if(s.contains("interface")) {
-				Interface=true;
-			}
-			if(s.contains("abstract")) {
-				abstrait=true;
-			}
-			if(s.contains("public")) {
-				if(s.contains("static")) {
-					
-				}
-				
-				/*RÈcupÈration de la ligne public class ...*/
-				if(s.contains("class")||s.contains("interface")) {
-					
-					/*Gerer les exceptions de la classe*/
-					if(s.contains("throws")) {
-						thro=s.substring(s.indexOf("throws")+6);
-						if(thro.contains("{")) {
-							thro=thro.substring(0,thro.indexOf("{"));
-						}
-						thro=thro.trim();
-						if(thro.contains(",")) {
-							thT=thro.split(",");
-							for(int i=0;i<thT.length;i++) {
-								throList.add(thT[i]);
-							}
-						}
-					}
-					/*Gerer les implÈmentation de la classe*/
-					if(s.contains("implements")) {
-						implement=true;
-						imple=s.substring(s.indexOf("implements")+10);
-						if(imple.contains(thro) && !thro.equals(" ")) {
-							imple=imple.substring(0,imple.indexOf(thro)-7);
-						}
-						if(imple.contains("{")) {
-							imple=imple.substring(0,imple.indexOf("{"));
-						}
-						imple=imple.trim();
-						if(imple.contains(",")) {
-							implT=imple.split(",");
-							for(int i=0;i<implT.length;i++) {
-								impleList.add(implT[i]);
-							}
-						}
-					}
-					/*GÈrer l'hÈritage de la classe*/
-					if(s.contains("extends")) {
-						etend=true;
-						extend=s.substring(s.indexOf("extends")+7);
-						extend=extend.trim();
-						if(extend.contains(thro)) {
-							extend=extend.substring(0,extend.indexOf(thro)-7);
-						}
-						if(extend.contains(imple)) {
-							extend=extend.substring(0,extend.indexOf(imple)-11);
-						}
-						if(extend.contains("{")) {
-							extend=extend.substring(0,extend.indexOf("{"));
-						}
-					}
-					/*RÈcupÈration du nom de la classe*/
-					if(extend!=" ") {
-						nomC=s.substring(s.indexOf("class")+5, s.indexOf(extend)-8);
-					}else if(imple!=" ") {
-						nomC=s.substring(s.indexOf("class")+5, s.indexOf(imple)-11);
-					}else if(thro!=" ") {
-						nomC=s.substring(s.indexOf("class")+5, s.indexOf(thro)-7);
-					}else {
-						nomC=s.substring(s.indexOf("class")+5);
-					}
-					if(nomC.contains("{")) {
-						nomC=nomC.substring(0,nomC.indexOf("{"));
-					}
-					System.out.println(nomC);
-					
-				}else {
-					/*RÈcupÈration des mÈthodes ou attributs*/
-					if(s.contains("{")) {
-						methodes.add(s.substring(0,s.indexOf("{")).trim());
-					}else {
-						if(s.contains(";")) {
-							attributs.add(s.substring(0,s.indexOf(";")).trim());
-						}
-					}
-				}
-				
-				
-			}else if(s.contains("private")) {
-				/*RÈcupÈration de la ligne public class ...*/
-				if(s.contains("class")||s.contains("interface")) {
-					
-					/*Gerer les exceptions de la classe*/
-					if(s.contains("throws")) {
-						thro=s.substring(s.indexOf("throws")+6);
-						if(thro.contains("{")) {
-							thro=thro.substring(0,thro.indexOf("{"));
-						}
-						thro=thro.trim();
-						if(thro.contains(",")) {
-							thT=thro.split(",");
-							for(int i=0;i<thT.length;i++) {
-								throList.add(thT[i]);
-							}
-						}
-					}
-					/*Gerer les implÈmentation de la classe*/
-					if(s.contains("implements")) {
-						imple=s.substring(s.indexOf("implements")+10);
-						if(imple.contains(thro)) {
-							imple=imple.substring(0,imple.indexOf(thro)-7);
-						}
-						if(imple.contains("{")) {
-							imple=imple.substring(0,imple.indexOf("{"));
-						}
-						imple=imple.trim();
-						if(imple.contains(",")) {
-							implT=imple.split(",");
-							for(int i=0;i<implT.length;i++) {
-								impleList.add(implT[i]);
-							}
-						}
-					}
-					/*GÈrer l'hÈritage de la classe*/
-					if(s.contains("extends")) {
-						extend=s.substring(s.indexOf("extends")+7);
-						extend=extend.trim();
-						if(extend.contains(thro)) {
-							extend=extend.substring(0,extend.indexOf(thro)-7);
-						}
-						if(extend.contains(imple)) {
-							extend=extend.substring(0,extend.indexOf(imple)-11);
-						}
-						if(extend.contains("{")) {
-							extend=extend.substring(0,extend.indexOf("{"));
-						}
-					}
-					/*RÈcupÈration du nom de la classe*/
-					if(extend!=" ") {
-						nomC=s.substring(s.indexOf("class")+5, s.indexOf(extend)-8);
-					}else if(imple!=" ") {
-						nomC=s.substring(s.indexOf("class")+5, s.indexOf(imple)-11);
-					}else if(thro!=" ") {
-						nomC=s.substring(s.indexOf("class")+5, s.indexOf(thro)-7);
-					}else {
-						nomC=s.substring(s.indexOf("class")+5);
-					}
-					if(nomC.contains("{")) {
-						nomC=nomC.substring(0,nomC.indexOf("{"));
-					}
-					System.out.println(nomC);
-					
-				}else {
-					/*RÈcupÈration des mÈthodes ou attributs*/
-					if(s.contains("{")) {
-						methodes.add(s.substring(0,s.indexOf("{")).trim());
-					}else {
-						if(s.contains(";")) {
-							attributs.add(s.substring(0,s.indexOf(";")).trim());
-						}
-					}
-				}
-			}else {
-				if(s.contains("package")) {
-					pack=s.substring(s.indexOf("package")+7);
-				}
-				
-			}
-		
-				
-			
-			/*Organisations des attributs*/
-			List<Argument> att=new LinkedList<>();
-			String[] tmp;
-			for(int i=0;i<attributs.size();i++) {
-				tmp=attributs.get(i).split(" ");
-				if(tmp[2].contains(";")) {
-					att.add(new Argument(tmp[2].substring(0, tmp[2].indexOf(";")),tmp[1],tmp[0]));
-				}else {
-					att.add(new Argument(tmp[2],tmp[1],tmp[0]));
-				}
-					
-				
-			}
-			/*Tri des mÈthodes*/
-			List<Methode> meth = new LinkedList<>();
-			List<Parametre> param = new LinkedList<>();
-			String tmpA;
-			String tmpB;
-			String tmp2;
-			String[] tP;
-			String exception;
-			for(int i=0;i<methodes.size();i++) {
-				
-				tmp=methodes.get(i).split(" ");
-				
-				tmp2=" ";
-				/* tri de la string / sÈparation des chaÓnes (espaces rajoutÈ ‡ certains endroits et parenthËse enlevÈ)*/
-				for(int j=0;j<tmp.length;j++) {
-					tmpB=null;
-					tmpA=null;
-					
-					
-					if(tmp[j].contains("(")) {
-						tmpA=tmp[j].substring(0, tmp[j].indexOf("("));
-						tmpB=tmp[j].substring(tmp[j].indexOf("(")+1);
-						if(tmp[j].contains(")")){
-							tmpB=null;
-							
-						}
-					}else if(tmp[j].contains(")")){
-						tmpA=tmp[j].substring(0, tmp[j].indexOf(")"));
-						
-					}
-					
-					
-					if(tmpB!=null) {
-						tmp2+= tmpA + " "+tmpB+" ";
-						
-					}else {
-						if(tmpA!=null) {
-							tmp2+= tmpA+" ";
-						}else {
-							tmp2+= tmp[j]+" ";
-						}
-					}
-					
-				}
-				/*SÈparation des exceptions si il y en a*/
-				exception="";
-				if(tmp2.contains("throws")) {
-					tP=tmp2.split(" ");
-					int j=0;
-					tmp2="";
-					while(!tP[j].equals("throws")) {
-						tmp2+=tP[j]+" ";
-						j++;
-					}
-					j++;
-					for(int k=j;k<tP.length;k++) {
-						exception+=tP[k]+" ";
-					}
-					
-				}
-				
-				
-				
-				
-				/* CrÈation du tableau de parametre */
-				if(tmp2.contains("abstract")) {
-					tP=tmp2.split(" ");
-					
-					if(tP.length>5) {
-						for(int j=4;j<tP.length;j+=2) {
-							param.add(new Parametre(tP[j],tP[j+1]));
-						}
-					}
-					if(tP[2].equals(nomC)) {
-						if(param.isEmpty()) {
-							if(!exception.equals("")) {
-								meth.add(new Methode("",tP[2],tP[1],exception.split(" ")));
-							}else {
-								meth.add(new Methode("",tP[2],tP[1]));
-							}
-						}else {
-							if(!exception.equals("")) {
-								meth.add(new Methode("",tP[2],param,tP[1],exception.split(" ")));
-							}else {
-								meth.add(new Methode("",tP[2],param,tP[1]));
-							}
-						}
-					}else {
-						if(param.isEmpty()) {
-							if(!exception.equals("")) {
-								meth.add(new Methode(tP[2],tP[3],tP[1],exception.split(" ")));
-							}else {
-								meth.add(new Methode(tP[2],tP[3],tP[1]));
-							}
-						}else {
-							if(!exception.equals("")) {
-								meth.add(new Methode(tP[2],tP[3],param,tP[1],exception.split(" ")));
-							}else {
-								meth.add(new Methode(tP[2],tP[3],param,tP[1]));
-							}
-						}
-					}
-					
-				}else {
-					tP=tmp2.split(" ");
-					
-					if(tP.length>4) {
-						for(int j=3;j<tP.length-1;j+=2) {
-							param.add(new Parametre(tP[j],tP[j+1]));
-						}
-					}
-					if(tP[2].equals(nomC)) {
-						if(param.isEmpty()) {
-							if(!exception.equals("")) {
-								meth.add(new Methode("",tP[2],tP[1],exception.split(" ")));
-							}else {
-								meth.add(new Methode("",tP[2],tP[1]));
-							}
-						}else {
-							if(!exception.equals("")) {
-								meth.add(new Methode("",tP[2],param,tP[1],exception.split(" ")));
-							}else {
-								meth.add(new Methode("",tP[2],param,tP[1]));
-							}
-						}
-					}else {
-						if(param.isEmpty()) {
-							if(!exception.equals("")) {
-								meth.add(new Methode(tP[2],tP[3],tP[1],exception.split(" ")));
-							}else {
-								meth.add(new Methode(tP[2],tP[3],tP[1]));
-							}
-						}else {
-							if(!exception.equals("")) {
-								meth.add(new Methode(tP[2],tP[3],param,tP[1],exception.split(" ")));
-							}else {
-								meth.add(new Methode(tP[2],tP[3],param,tP[1]));
-							}
-						}
-					}
-				}
-				param.clear();
-			}
-			
-			sc.close();
-			Classe cl=new Classe(nomC,meth,att,etend,implement,pack,Interface,abstrait);
-			return cl;
-		}
-		} catch (FileNotFoundException e) {
-			
-			e.printStackTrace();
-			
-		}
-		return null;
-		
 	}
 
 
--- TestSVG/test/testLien.java
+++ TestSVG/test/testLien.java
@@ -34,7 +34,7 @@ public class testLien {
         assertEquals("0",li.getMultipliciteF());
     }
     @Test
-    public void testMultiplicité(){
+    public void testMultiplicitÃ©(){
         Lien li = new Lien(matiere);
         li.presenceLien(groupe);
         assertEquals("0",li.getMultipliciteD());
